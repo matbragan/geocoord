d353ff016fe907eb8df0a8e577d0195afcb0a0c5
matbragan <matheus.bs.oliveira@gmail.com>
2024-01-21 12:14:23 -0300

    update Python API, change method & body request

--- coordinates_api.py
+++ coordinates_api.py
@@ -3,20 +3,15 @@ from geopy.geocoders import Nominatim
 
 app = Flask(__name__)
 
-@app.route('/get_coordinates', methods=['POST'])
+@app.route('/get_coordinates', methods=['GET'])
 def get_coordinates():
-    data = request.get_json()
-
-    cep = data.get('cep')
-    street = data.get('street')
-    neighborhood = data.get('neighborhood')
-    city = data.get('city')
-    state = data.get('state')
-
-    address = f'{street} {neighborhood} {city} {state}'
-
     geolocator = Nominatim(user_agent='geoapi')
-    location = geolocator.geocode(cep) or geolocator.geocode(address)
+    
+    data = request.get_json()
+    zip_code = data.get('zip_code')
+
+    if zip_code:
+        location = geolocator.geocode(zip_code)
 
     try:
         coordinates = {
